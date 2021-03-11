d73f619f9b2640ed8cce5eaeebf36ac206a2799e
Dominik-domj-Jancik <hi@domj.net>
2021-03-11 21:06:48 +0100

    wip

--- ch8-collections/pig_latin/src/main.rs
+++ ch8-collections/pig_latin/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
     println!("Feed me words, oink!");
+
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).expect("Read ailed-fay :(");
+        let words = line.split_whitespace();
+
+        for word in words {
+            let first_letter = word.chars()[0]
+            println!("{}", first_letter);
+        }
+    }
 }
