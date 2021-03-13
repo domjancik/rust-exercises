cf20b9c04004fb0a92ed5ed4ec8cb7e424714e51
Dominik-domj-Jancik <hi@domj.net>
2021-03-13 17:39:18 +0100

    detecting consonants and vowels, exercise completed

--- ch8-collections/pig_latin/src/main.rs
+++ ch8-collections/pig_latin/src/main.rs
@@ -13,9 +13,20 @@ fn main() {
             let first_letter = chars.next().expect("Empty word, oops...");
             // .collect(), important!
             let rest: String = chars.collect();
-
-            // TODO check for vowel
-            println!("{}-{}ay", rest, first_letter);
+            match first_letter {
+                // | allows matching multiple cases at once. Also ranges usinng .. are supported.
+                // https://doc.rust-lang.org/rust-by-example/flow_control/match.html
+                'a' | 'e' | 'i' | 'o' | 'u' => transform_vowel_word(&first_letter, &rest),
+                _ => transform_consonant_word(&first_letter, &rest)
+            }
         }
     }
+}
+
+fn transform_vowel_word(first_letter: &char, rest: &String) {
+    println!("{}{}-hay", first_letter, rest);
+}
+
+fn transform_consonant_word(first_letter: &char, rest: &String) {
+    println!("{}-{}ay", rest, first_letter);
 }
